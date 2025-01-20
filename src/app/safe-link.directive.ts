ca2a2cb827c8cfad4811171698276707f65f658d
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-01-20 11:49:18 -0600

    Mejorando la directiva y era de atributos

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -3,10 +3,20 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true,
-  host: {},
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
 export class SafeLinkdirective {
   constructor() {
     console.info('Estoy funcionando');
+  }
+  onConfirmLeavePage(event: MouseEvent) {
+    const leavePage = window.confirm('¿Quieres abandonar la página?');
+
+    if (leavePage) {
+      return;
+    }
+    event?.preventDefault();
   }
 }
