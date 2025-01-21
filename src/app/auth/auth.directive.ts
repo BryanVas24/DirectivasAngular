f21a5d3f3a06a51dba91e0fd822ce7a53dd89cc5
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-01-21 13:03:45 -0600

    Construyendo una directiva estructural

--- src/app/auth/auth.directive.ts
+++ src/app/auth/auth.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, effect, inject, Input } from '@angular/core';
+import {
+  Directive,
+  effect,
+  inject,
+  Input,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 import { Permission } from './auth.model';
 import { AuthService } from './auth.service';
 
@@ -9,12 +16,17 @@ import { AuthService } from './auth.service';
 export class AuthDirective {
   @Input({ required: true, alias: 'appAuth' }) userAcces!: Permission;
   private authService = inject(AuthService);
+  //referencia a la plantilla donde se esta usando,
+  private templateRef = inject(TemplateRef);
+  //te da acceso a el lugar donde se referencia en le DOm
+  private viewContainerRef = inject(ViewContainerRef);
   constructor() {
     effect(() => {
       if (this.authService.activePermission() === this.userAcces) {
-        console.log('Mostraaaaas');
+        //Toma el contenido de el ng-template
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
       } else {
-        console.log('No mostraaaaas');
+        this.viewContainerRef.clear();
       }
     });
   }
